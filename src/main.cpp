c564ced743d59bccaa2cb5d373eabc572755d244
Håvard Kindem <havard.kindem@gmail.com>
2019-02-15 15:46:42 +0100

    Moved to GLFW

--- src/main.cpp
+++ src/main.cpp
@@ -5,20 +5,24 @@
 #include <string>
 #include <GL/glew.h>
 #include <GL/gl.h>
+#include <GL/glu.h>
 #include <GLFW/glfw3.h>
 
 #include "util.h"
 
 using namespace std;
 
-
+static void keyCallback(GLFWwindow* window, int key, int scancode, int action, int mods) {
+    if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+        glfwSetWindowShouldClose(window, GLFW_TRUE);
+}
 
 int main(int argc, char* args[]) {
     GLFWwindow* window;
     if (!glfwInit())
         return -1;
 
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    window = glfwCreateWindow(640, 480, "GLFW Playground", NULL, NULL);
     if (!window)
     {
         glfwTerminate();
@@ -26,6 +30,7 @@ int main(int argc, char* args[]) {
     }
 
     glfwMakeContextCurrent(window);
+    glfwSetKeyCallback(window, keyCallback);
 
     GLenum err = glewInit();
     if (err != GLEW_OK) {
@@ -33,24 +38,52 @@ int main(int argc, char* args[]) {
         return 1;
     }
     fprintf(stdout, "Status: Using GLEW %s\n", glewGetString(GLEW_VERSION));
+        
 
     GLuint prog = createProgram("shaders/test.vert", "shaders/test.frag");
 
+    int width, height;
+    glfwGetFramebufferSize(window, &width, &height);
+    glViewport(0, 0, width, height);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    gluPerspective(45.0f,(GLfloat)width/(GLfloat)height,0.1f,100.0f);
+ 
+    glMatrixMode(GL_MODELVIEW);
+    glLoadIdentity();  
+
     
-    //glUseProgram(prog);
+    float points[] = {
+    0.0f,  0.5f,  0.0f,
+    0.5f, -0.5f,  0.0f,
+    -0.5f, -0.5f,  0.0f
+    };
+
+    GLuint vbo = 0;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, 9 * sizeof(float), points, GL_STATIC_DRAW);
+
+    GLuint vao = 0;
+    glGenVertexArrays(1, &vao);
+    glBindVertexArray(vao);
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+    glTranslatef(0.0f, 0.0f, -5.0f);
 
     while (!glfwWindowShouldClose(window))
     {
         /* Render here */
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+        glUseProgram(prog);
 
-        /* Swap front and back buffers */
+        glBindVertexArray(vao);
+        glDrawArrays(GL_TRIANGLES, 0, 3);
+
         glfwSwapBuffers(window);
-
-        /* Poll for and process events */
-        glfwPollEvents();
-
-        
+        glfwPollEvents();   
     }
 
     glfwTerminate();
